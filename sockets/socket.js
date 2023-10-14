ff4c3359f6f5a38efb1d317bc49d28d97b21973d
Edwin Rivera <roedwin92@gmail.com>
2023-10-14 13:51:45 -0600

    Socket funcional

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
-const { io } = require('../index')
+const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('FMLN'));
+bands.addBand(new Band('Nuevas Ideas'));
+bands.addBand(new Band('Arena'));
+bands.addBand(new Band('PCN'));
+
 
 //mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands())
+
     client.on('disconnect', ()=>{
         console.log('Cliente desconectado')
     });
@@ -12,4 +25,27 @@ io.on('connection', client => {
 
         io.emit('mensaje', {admin: 'Nuevo mensaje'})
     });
+
+    client.on('vote-band', (payload)=> {
+        //console.log(payload.id)
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) =>{
+        console.log(payload);
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) =>{
+        
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     console.log(payload);
+    //     io.emit('nuevo-mensaje', payload);
+    // });
 });
